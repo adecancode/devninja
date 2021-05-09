ccf0d29ad2b9a068710976ee4acabb2111979e88
Adebayo Temitope <adecancode@gmail.com>
2021-05-09 18:27:06 +0100

    +

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" 
+          rel="stylesheet"
+        />
+        <title>Ade / Front End & Mobile Developer</title>
+        <meta name="description" content="Ade's Portfolio" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
